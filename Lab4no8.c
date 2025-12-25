f1538f8ad49ff1cf168c9df625967ef8c101371a
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2025-12-26 00:04:16 +0700

    N grade Bonus1+Bonus2

---
+++ Lab4no8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int score,student,i;
+    scanf("%d",&student);
+    for(i = 1;i<=student;i++){
+        scanf("%d",&score);
+        if(score<=100){
+            if(score>=68){
+                if(score>=87){
+                    printf("A");
+                }else if(score>=79){
+                    printf("B");
+                }else if(score>=71){
+                    printf("C\n");
+                    printf("B is %d",79-score);
+                }
+            
+            }else{
+                if(score<=63){
+                    if(score>12){
+                        printf("D");
+                    }else if(score<0){
+                        printf("error score");
+                    }else{
+                        printf("F");
+                    }
+                }
+            }
+        }else{
+            printf("error score");
+        }
+        printf("\n");
+    }
+    return 0;
+}
